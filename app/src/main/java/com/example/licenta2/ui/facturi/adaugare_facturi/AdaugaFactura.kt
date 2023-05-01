5d2901223b5c8d152e84e893df3e7333de36b293
dumitriu-ana <anadumitriu68@gmail.com>
2023-05-01 18:38:14 +0300

    rv cu listaProduse in adaugareFactura

--- app/src/main/java/com/example/licenta2/ui/facturi/adaugare_facturi/AdaugaFactura.kt
+++ app/src/main/java/com/example/licenta2/ui/facturi/adaugare_facturi/AdaugaFactura.kt
@@ -3,7 +3,6 @@ package com.example.licenta2.ui.facturi.adaugare_facturi
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +12,8 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.licenta2.R
 import com.example.licenta2.databinding.FragmentAdaugaFacturaBinding
 import com.example.licenta2.persistence.database.AppDatabase
@@ -47,6 +48,8 @@ class AdaugaFactura : Fragment(), CellClickListener {
     private val binding get() = _binding!!
 
     private lateinit var appDatabase: AppDatabase
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var listaProduseFinaleAdaptor: ListaFinalaProdAdaptor
 
     var client: Client? = null
 
@@ -81,8 +84,17 @@ class AdaugaFactura : Fragment(), CellClickListener {
             findNavController().navigate(R.id.action_adaugaFactura_to_selectareProdusFragment)
         }
 
+
+        recyclerView = binding.rvFacturaListaProduse
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        listaProduseFinaleAdaptor = ListaFinalaProdAdaptor(requireContext(), emptyList())
+        recyclerView.adapter = listaProduseFinaleAdaptor
+
         sharedViewModel.listaFinalaProduse.observe(viewLifecycleOwner) {
             Toast.makeText(requireContext(), "Lista p  $it", Toast.LENGTH_LONG).show()
+
+            listaProduseFinaleAdaptor = ListaFinalaProdAdaptor(requireContext(), it)
+            recyclerView.adapter = listaProduseFinaleAdaptor
         }
 
 
---
+++ app/src/main/java/com/example/licenta2/ui/facturi/adaugare_facturi/ListaFinalaProdAdaptor.kt
@@ -0,0 +1,42 @@
+package com.example.licenta2.ui.facturi.adaugare_facturi
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.example.licenta2.R
+import com.example.licenta2.persistence.entities.Factura
+import com.example.licenta2.persistence.entities.Produs
+
+
+class ListaFinalaProdAdaptor(private val context: Context, private var produse: List<Produs>) : RecyclerView.Adapter<ListaFinalaProdAdaptor.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.lv_row_lista_finala_produse, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produs = produse[position]
+
+        holder.produsDenumire.text = produs.denumire
+        holder.produsPret.text = produs.pret.toString()
+       // holder.produsCantitate.text = "1"
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return produse.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+       val produsDenumire:TextView = itemView.findViewById(R.id.denumireTextView)
+        val produsPret:TextView = itemView.findViewById(R.id.pretTextView)
+        val produsCantitate:EditText = itemView.findViewById(R.id.cantitateEditText)
+
+    }
+}
+
