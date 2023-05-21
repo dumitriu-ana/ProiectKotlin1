e3f30b165f368cbf8d325e27b6f7ea6d2cfc7967
dumitriu-ana <anadumitriu68@gmail.com>
2023-05-22 00:40:16 +0300

    trimitere feedback

--- app/src/main/java/com/example/licenta2/ui/logout/LogoutFragment.kt
+++ app/src/main/java/com/example/licenta2/ui/logout/LogoutFragment.kt
@@ -1,12 +1,14 @@
 package com.example.licenta2.ui.logout
 
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.licenta2.FirebaseInregistrare
 import com.example.licenta2.R
 import com.example.licenta2.databinding.FragmentLoginBinding
@@ -33,8 +35,24 @@ class LogoutFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.buttonFeedback.setOnClickListener {
+            trimiteFeedbackEmail()
+        }
 
         return root
+    }
+
+    private fun trimiteFeedbackEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:dumitriuana288@gmail.com")
+            putExtra(Intent.EXTRA_SUBJECT, "Feedback Liliana")
+            putExtra(Intent.EXTRA_TEXT, "Mesaj feedback: ")
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), "Nu există aplicații de email instalate.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
